8c46f37311fbb92ff55bcc21097521d30a141f63
zhouru1bin <ruibin@simplylab.xyz>
2023-05-04 11:13:42 +0800

    fix: markdown style

--- src/components/CustomMarkdown/index.tsx
+++ src/components/CustomMarkdown/index.tsx
@@ -1,7 +1,8 @@
-import Markdown from 'markdown-to-jsx'
+import Markdown, { Options } from 'markdown-to-jsx'
 import React, { FC } from 'react'
-import { Link } from '@mui/material'
+import { Box, Link, Stack, Typography } from '@mui/material'
 import { chromeExtensionClientOpenPage, CLIENT_OPEN_PAGE_KEYS } from '@/utils'
+import CopyTooltipIconButton from '../CopyTooltipIconButton'
 
 const OverrideAnchor: FC<HTMLAnchorElement> = (props) => {
   if (props.href?.startsWith('key=')) {
@@ -31,6 +32,44 @@ const OverrideAnchor: FC<HTMLAnchorElement> = (props) => {
   )
 }
 
+const OverrideCode: FC<HTMLElement> = (props) => {
+  return (
+    <Stack
+      bgcolor="#000"
+      sx={{
+        borderRadius: '6px',
+        mb: 2,
+        overflow: 'hidden',
+      }}
+    >
+      <Stack
+        justifyContent="flex-end"
+        itemsAlign="center"
+        direction="row"
+        px={2}
+        py={0.5}
+        sx={{
+          bgcolor: 'rgba(52,53,65,1)',
+          color: 'rgb(217,217,227)',
+        }}
+      >
+        <CopyTooltipIconButton
+          copyText={props.children as any}
+          sx={{
+            borderRadius: '6px',
+            px: '8px !important',
+          }}
+        >
+          <span style={{ marginLeft: '4px', fontSize: '12px' }}>Copy code</span>
+        </CopyTooltipIconButton>
+      </Stack>
+      <Box p={2} color="#FFFFFFDE">
+        {props.children as any}
+      </Box>
+    </Stack>
+  )
+}
+
 const CustomMarkdown: FC<{
   children: string
 }> = (props) => {
@@ -39,6 +78,7 @@ const CustomMarkdown: FC<{
       options={{
         overrides: {
           a: OverrideAnchor,
+          code: OverrideCode,
         },
       }}
     >

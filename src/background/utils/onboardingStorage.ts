bd585214100ac05362e69fa1a14c9f8595d78c77
yanggerSimply <chenyang@simplylab.xyz>
2023-05-29 12:36:41 +0800

    feta: BrowserVersionDetector

--- src/background/utils/onboardingStorage.ts
+++ src/background/utils/onboardingStorage.ts
@@ -1,7 +1,9 @@
 import { CHROME_EXTENSION_LOCAL_ON_BOARDING_SAVE_KEY } from '@/types'
 import Browser from 'webextension-polyfill'
 
-type OnBoardingKeyType = 'ON_BOARDING_RECORD_FIRST_MESSAGE'
+type OnBoardingKeyType =
+  | 'ON_BOARDING_RECORD_FIRST_MESSAGE'
+  | 'ON_BOARDING_RECORD_BROWSER_VERSION'
 
 type OnBoardingMapType = {
   [key in OnBoardingKeyType]?: boolean | string | number
@@ -9,6 +11,7 @@ type OnBoardingMapType = {
 const defaultOnBoardingMap: OnBoardingMapType = {
   // 记录用户第一次发送的message
   ON_BOARDING_RECORD_FIRST_MESSAGE: false,
+  ON_BOARDING_RECORD_BROWSER_VERSION: false,
 }
 
 /**

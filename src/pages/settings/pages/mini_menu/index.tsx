eb072a9becdc5a3530cee5a1355e49e74357be6c
yanggerSimply <chenyang@simplylab.xyz>
2023-07-28 20:07:01 +0800

    chore: fix mini menu.

--- src/pages/settings/pages/mini_menu/index.tsx
+++ src/pages/settings/pages/mini_menu/index.tsx
@@ -1,5 +1,5 @@
 import SettingsFeatureCardLayout from '@/pages/settings/layout/SettingsFeatureCardLayout'
-import React, { FC, useMemo } from 'react'
+import React, { FC, useMemo, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import Stack from '@mui/material/Stack'
 import RadioCardGroup from '@/pages/settings/components/RadioCardGroup'
@@ -11,6 +11,7 @@ import {
 } from '@/background/utils/buttonSettings'
 import VisibilitySettingCard from '@/components/VisibilitySettingCard'
 import cloneDeep from 'lodash-es/cloneDeep'
+import { useFocus } from '@/hooks/useFocus'
 
 const SettingsMiniMenuPage: FC = () => {
   const { toggleButtonSettings, updateButtonSettings } =
@@ -18,70 +19,81 @@ const SettingsMiniMenuPage: FC = () => {
   const buttonSettings = useComputedChromeExtensionButtonSettings(
     'textSelectPopupButton',
   )
+  const [reRender, setReRender] = useState(true)
   const visible = useMemo(() => {
-    return buttonSettings?.visibility.isWhitelistMode !== true
+    return buttonSettings?.visibility.isWhitelistMode === false
   }, [buttonSettings?.visibility])
+  useFocus(() => {
+    setReRender(false)
+    setTimeout(() => {
+      setReRender(true)
+    }, 50)
+  })
   const { t } = useTranslation(['settings'])
   return (
     <SettingsFeatureCardLayout
       title={t('feature_card__mini_menu__title')}
       id={'mini-menu'}
     >
-      <Stack spacing={2}>
-        <RadioCardGroup
-          defaultValue={visible ? 'visible' : 'hidden'}
-          options={[
-            {
-              label: t(
-                'settings:feature_card__mini_menu__field_mini_menu__visible',
-              ),
-              value: 'visible',
-              image: getChromeExtensionAssetsURL(
-                '/images/settings/appearance/mini-menu-visible.png',
-              ),
-            },
-            {
-              label: t(
-                'settings:feature_card__mini_menu__field_mini_menu__hidden',
-              ),
-              value: 'hidden',
-              image: getChromeExtensionAssetsURL(
-                '/images/settings/appearance/mini-menu-hidden.png',
-              ),
-            },
-          ]}
-          onChange={async (value) => {
-            if (value === 'visible') {
-              // 从白名单模式切换到黑名单模式
-              await toggleButtonSettings('textSelectPopupButton', true)
-            } else {
-              // 从黑名单模式切换到白名单模式
-              await toggleButtonSettings('textSelectPopupButton', false)
-            }
-          }}
-          maxWidth={372}
-        />
-        {visible && buttonSettings?.visibility && (
-          <VisibilitySettingCard
-            mode={'black'}
-            defaultValue={buttonSettings.visibility}
+      {buttonSettings?.visibility && reRender && (
+        <Stack spacing={2}>
+          <RadioCardGroup
+            defaultValue={visible ? 'visible' : 'hidden'}
+            options={[
+              {
+                label: t(
+                  'settings:feature_card__mini_menu__field_mini_menu__visible',
+                ),
+                value: 'visible',
+                image: getChromeExtensionAssetsURL(
+                  '/images/settings/appearance/mini-menu-visible.png',
+                ),
+              },
+              {
+                label: t(
+                  'settings:feature_card__mini_menu__field_mini_menu__hidden',
+                ),
+                value: 'hidden',
+                image: getChromeExtensionAssetsURL(
+                  '/images/settings/appearance/mini-menu-hidden.png',
+                ),
+              },
+            ]}
             onChange={async (value) => {
-              const newVisibility = cloneDeep(value)
-              if (newVisibility.isWhitelistMode) {
-                newVisibility.whitelist = []
-              }
-              const latestButtonSettings =
-                await getChromeExtensionButtonSettings('textSelectPopupButton')
-              if (latestButtonSettings) {
-                await updateButtonSettings('textSelectPopupButton', {
-                  ...latestButtonSettings,
-                  visibility: value,
-                })
+              if (value === 'visible') {
+                // 从白名单模式切换到黑名单模式
+                await toggleButtonSettings('textSelectPopupButton', true)
+              } else {
+                // 从黑名单模式切换到白名单模式
+                await toggleButtonSettings('textSelectPopupButton', false)
               }
             }}
-          ></VisibilitySettingCard>
-        )}
-      </Stack>
+            maxWidth={372}
+          />
+          {visible && (
+            <VisibilitySettingCard
+              mode={'black'}
+              defaultValue={buttonSettings.visibility}
+              onChange={async (value) => {
+                const newVisibility = cloneDeep(value)
+                if (newVisibility.isWhitelistMode) {
+                  newVisibility.whitelist = []
+                }
+                const latestButtonSettings =
+                  await getChromeExtensionButtonSettings(
+                    'textSelectPopupButton',
+                  )
+                if (latestButtonSettings) {
+                  await updateButtonSettings('textSelectPopupButton', {
+                    ...latestButtonSettings,
+                    visibility: value,
+                  })
+                }
+              }}
+            ></VisibilitySettingCard>
+          )}
+        </Stack>
+      )}
     </SettingsFeatureCardLayout>
   )
 }

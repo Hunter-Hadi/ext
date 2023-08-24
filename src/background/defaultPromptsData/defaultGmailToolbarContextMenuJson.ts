66c920ff91c2efdd0b203339c944b5d425682206
yanggerSimply <chenyang@simplylab.xyz>
2023-08-24 14:49:04 +0800

    pref: update gmail cta button prompts.

--- src/background/defaultPromptsData/defaultGmailToolbarContextMenuJson.ts
+++ src/background/defaultPromptsData/defaultGmailToolbarContextMenuJson.ts
@@ -61,7 +61,7 @@ Respond in {{AI_RESPONSE_LANGUAGE}}.
 
 Text:
 \`\`\`
-{{SELECTED_TEXT}}
+{{GMAIL_DRAFT_CONTEXT}}
 \`\`\`
 
 Your task requires you to write a concise email reply in a polite, friendly, professional, and proper way.

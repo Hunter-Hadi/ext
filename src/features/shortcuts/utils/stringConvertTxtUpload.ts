ec5bd73529ea17c785ee668a54e3620b17b1923e
yanggerSimply <i@yangger.cn>
2023-09-08 19:52:59 +0800

    chore: add larkbot.

--- src/features/shortcuts/utils/stringConvertTxtUpload.ts
+++ src/features/shortcuts/utils/stringConvertTxtUpload.ts
@@ -1,6 +1,7 @@
 import { APP_USE_CHAT_GPT_API_HOST } from '@/constants'
 import { getAccessToken } from '@/utils/request'
 import { md5TextEncrypt } from '@/utils/encryptionHelper'
+import { sendLarkBotMessage } from '@/utils/larkBot'
 
 export const MaxUploadTxtFileSize = 1024 * 1024 * 32 // 32MB
 export const MaxUploadTxtFileTokens = 400 * 1000 // 400k
@@ -26,10 +27,39 @@ export const stringConvertTxtUpload = async (
     })
       .then((response) => response.json())
       .then((result) => {
-        resolve(result?.data?.doc_id || '')
+        const docId = result?.data?.doc_id || ''
+        if (!docId) {
+          sendLarkBotMessage(
+            '[API] [/gpt/analyze_file] error',
+            JSON.stringify(
+              {
+                error: 'response docId is empty',
+              },
+              null,
+              2,
+            ),
+            {
+              uuid: '6f02f533-def6-4696-b14e-1b00c2d9a4df',
+            },
+          )
+        }
+        resolve(docId)
       })
       .catch((error) => {
         console.error('文件上传错误:', error)
+        sendLarkBotMessage(
+          '[API] [/gpt/analyze_file] error',
+          JSON.stringify(
+            {
+              error: error?.message || error?.toString() || 'unknown error',
+            },
+            null,
+            2,
+          ),
+          {
+            uuid: '6f02f533-def6-4696-b14e-1b00c2d9a4df',
+          },
+        )
         resolve('')
       })
   })

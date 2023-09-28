fc4fe2955ecbcdd375f51d057f9c3bf86ebcb4b0
yanggerSimply <i@yangger.cn>
2023-09-28 15:23:57 +0800

    chore: save changed.

--- src/pages/settings/pages/help_me_write/FeatureYouTubeAssistantCard.tsx
+++ src/pages/settings/pages/help_me_write/FeatureYouTubeAssistantCard.tsx
@@ -11,7 +11,7 @@ const FeatureYouTubeAssistantCard: FC = () => {
   return (
     <SettingsFeatureCardLayout
       title={t(
-        'settings:feature_card__help_me_write__field__facebook_input_assistant_button__title',
+        'settings:feature_card__help_me_write__field__youtube_input_assistant_button__title',
       )}
       id={'facebook-assistant'}
     >

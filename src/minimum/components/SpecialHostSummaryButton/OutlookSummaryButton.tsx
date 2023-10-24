1ab683e1414f47c58a161ada9af2eac2d350dbb1
yanggerSimply <i@yangger.cn>
2023-10-24 16:48:33 +0800

    feta: outlook summary button.

---
+++ src/minimum/components/SpecialHostSummaryButton/OutlookSummaryButton.tsx
@@ -0,0 +1,59 @@
+import React, { FC } from 'react'
+import DynamicComponent from '@/components/DynamicComponent'
+import Button from '@mui/material/Button'
+import useFindElement from '@/hooks/useFindElement'
+import { UseChatGptIcon } from '@/components/CustomIcon'
+import { useTranslation } from 'react-i18next'
+import { getAppRootElement, showChatBox } from '@/utils'
+import { ISidebarConversationType } from '@/features/sidebar/store'
+import Stack from '@mui/material/Stack'
+
+const OutlookSummaryButton: FC = () => {
+  const { t } = useTranslation(['client'])
+  const { element } = useFindElement('#ReadingPaneContainerId .Y0qxw')
+  return (
+    <DynamicComponent
+      rootContainer={element}
+      customElementName={'max-ai-outlook-summary-button'}
+    >
+      <Button
+        sx={{
+          padding: '3.5px 8px',
+          mr: 1,
+          borderRadius: '16px',
+        }}
+        variant={'contained'}
+        onClick={() => {
+          showChatBox()
+          const timer = setInterval(() => {
+            if (
+              getAppRootElement()?.querySelector(
+                'p[data-testid="max-ai__summary-tab"]',
+              )
+            ) {
+              clearInterval(timer)
+              window.dispatchEvent(
+                new CustomEvent('MaxAISwitchSidebarTab', {
+                  detail: {
+                    type: 'Summary' as ISidebarConversationType,
+                  },
+                }),
+              )
+            }
+          }, 500)
+        }}
+      >
+        <Stack direction={'row'} alignItems={'center'} gap={1}>
+          <UseChatGptIcon
+            sx={{
+              fontSize: 14,
+              color: 'inherit',
+            }}
+          />
+          {t('client:social_media_summary_button__text')}
+        </Stack>
+      </Button>
+    </DynamicComponent>
+  )
+}
+export default OutlookSummaryButton
--- src/minimum/components/SpecialHostSummaryButton/index.tsx
+++ src/minimum/components/SpecialHostSummaryButton/index.tsx
@@ -6,6 +6,7 @@ import {
   isMaxAIPDFPage,
 } from '@/utils/dataHelper/websiteHelper'
 import PDFSummaryButton from '@/minimum/components/SpecialHostSummaryButton/PDFSummaryButton'
+import OutlookSummaryButton from '@/minimum/components/SpecialHostSummaryButton/OutlookSummaryButton'
 
 const SpecialHostSummaryButton: FC = () => {
   const [appHost] = useState(getCurrentDomainHost)
@@ -14,6 +15,7 @@ const SpecialHostSummaryButton: FC = () => {
       {appHost === 'mail.google.com' && <GmailSummaryButton />}
       {appHost === 'youtube.com' && <YouTubeSummaryButton />}
       {isMaxAIPDFPage() && <PDFSummaryButton />}
+      {appHost === 'outlook.live.com' && <OutlookSummaryButton />}
     </>
   )
 }

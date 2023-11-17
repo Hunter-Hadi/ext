7030dd3f910bd48f86b7ea8ec301fdb90d73eeb9
yanggerSimply <i@yangger.cn>
2023-11-17 18:28:19 +0800

    chore: save changed.

--- src/features/prompt_library/components/PromptLibraryIconButton.tsx
+++ src/features/prompt_library/components/PromptLibraryIconButton.tsx
@@ -3,24 +3,51 @@ import TextOnlyTooltip from '@/components/TextOnlyTooltip'
 import Button from '@mui/material/Button'
 import { MagicBookIcon } from '@/components/CustomIcon'
 import { useTranslation } from 'react-i18next'
+import Popper from '@mui/material/Popper'
+import { Fade, PopperPlacementType } from '@mui/material'
+import Paper from '@mui/material/Paper'
+import Typography from '@mui/material/Typography'
 
 const PromptLibraryIconButton: FC = () => {
   const { t } = useTranslation(['prompt_library'])
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
+  const [open, setOpen] = React.useState(false)
+  const [placement, setPlacement] = React.useState<PopperPlacementType>()
+  const handleClick = (newPlacement: PopperPlacementType) => (
+    event: React.MouseEvent<HTMLButtonElement>,
+  ) => {
+    setAnchorEl(event.currentTarget)
+    setOpen((prev) => placement !== newPlacement || !prev)
+    setPlacement(newPlacement)
+  }
+
   return (
-    <TextOnlyTooltip
-      placement={'top'}
-      title={t('prompt_library:use_prompt_library__title')}
-    >
-      <Button
-        sx={{
-          p: '5px',
-          minWidth: 'unset',
-        }}
-        variant={'outlined'}
+    <>
+      <TextOnlyTooltip
+        placement={'top'}
+        title={t('prompt_library:use_prompt_library__title')}
       >
-        <MagicBookIcon sx={{ fontSize: '20px', color: 'primary.main' }} />
-      </Button>
-    </TextOnlyTooltip>
+        <Button
+          onClick={handleClick('top')}
+          sx={{
+            p: '5px',
+            minWidth: 'unset',
+          }}
+          variant={'outlined'}
+        >
+          <MagicBookIcon sx={{ fontSize: '20px', color: 'primary.main' }} />
+        </Button>
+      </TextOnlyTooltip>
+      <Popper open={open} anchorEl={anchorEl} placement={placement} transition>
+        {({ TransitionProps }) => (
+          <Fade {...TransitionProps} timeout={350}>
+            <Paper>
+              <Typography sx={{ p: 2 }}>The content of the Popper.</Typography>
+            </Paper>
+          </Fade>
+        )}
+      </Popper>
+    </>
   )
 }
 export default PromptLibraryIconButton

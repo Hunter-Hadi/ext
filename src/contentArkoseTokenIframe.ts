59fba53611f5bc49683ee14d498b4e3275a6b0d6
yanggerSimply <i@yangger.cn>
2023-11-30 16:28:46 +0800

    pref: arkose token generator.

--- src/contentArkoseTokenIframe.ts
+++ src/contentArkoseTokenIframe.ts
@@ -13,26 +13,28 @@ const isInIframe = () => {
   }
 }
 
-window.onload = () => {
-  if (window.location.origin === 'https://chat.openai.com' && isInIframe()) {
-    // 1. 注入windowArkoseTokenIFrame
-    const script = document.createElement('script')
-    script.src = Browser.runtime.getURL(
-      'assets/openai/windowArkoseTokenIframe.js',
-    )
-    script.type = 'module'
-    script.setAttribute(
-      'data-arkose_token_gpt_3_5',
-      Browser.runtime.getURL(
-        'assets/openai/js/v2/3D86FBBA-9D22-402A-B512-3420086BA6CC/api.js',
-      ),
-    )
-    script.setAttribute(
-      'data-arkose_token_gpt_4',
-      Browser.runtime.getURL(
-        'assets/openai/js/v2/35536E1E-65B4-4D96-9D97-6ADB7EFF8147/api.js',
-      ),
-    )
-    document.body.appendChild(script)
-  }
+if (
+  window.location.origin === 'https://chat.openai.com' &&
+  isInIframe() &&
+  document?.head?.appendChild
+) {
+  // 1. 注入windowArkoseTokenIFrame
+  const script = document.createElement('script')
+  script.src = Browser.runtime.getURL(
+    'assets/openai/windowArkoseTokenIframe.js',
+  )
+  script.type = 'module'
+  script.setAttribute(
+    'data-arkose_token_gpt_3_5',
+    Browser.runtime.getURL(
+      'assets/openai/js/v2/3D86FBBA-9D22-402A-B512-3420086BA6CC/api.js',
+    ),
+  )
+  script.setAttribute(
+    'data-arkose_token_gpt_4',
+    Browser.runtime.getURL(
+      'assets/openai/js/v2/35536E1E-65B4-4D96-9D97-6ADB7EFF8147/api.js',
+    ),
+  )
+  document.head.appendChild(script)
 }

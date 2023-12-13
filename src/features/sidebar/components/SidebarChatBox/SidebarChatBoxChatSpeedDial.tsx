058af821ee9b8aeb2273761f0f62e2bffabaced5
yanggerSimply <i@yangger.cn>
2023-12-13 17:01:47 +0800

    fix: bugs.

--- src/features/sidebar/components/SidebarChatBox/SidebarChatBoxChatSpeedDial.tsx
+++ src/features/sidebar/components/SidebarChatBox/SidebarChatBoxChatSpeedDial.tsx
@@ -129,42 +129,43 @@ const SidebarChatBoxChatSpeedDial: FC<{
           />
         )}
         {currentSidebarConversationType === 'Chat' && (
-          &&<SpeedDialAction
-          icon={
-            <Box
-              component={'div'}
-              onClick={(event: any) => {
-                event.stopPropagation()
-                //  data-testid={'maxai-prompt-library-button'}
-                const button = getMaxAISidebarRootElement()?.querySelector(
-                  '[data-testid="maxai-prompt-library-button"]',
-                ) as HTMLButtonElement
-                if (button) {
-                  button.click()
-                }
-              }}
-            >
-              <TextOnlyTooltip
-                placement={'left'}
-                title={t(
-                  'client:sidebar__speed_dial__one_click_prompts__button',
-                )}
+          <SpeedDialAction
+            icon={
+              <Box
+                component={'div'}
+                onClick={(event: any) => {
+                  event.stopPropagation()
+                  //  data-testid={'maxai-prompt-library-button'}
+                  const button = getMaxAISidebarRootElement()?.querySelector(
+                    '[data-testid="maxai-prompt-library-button"]',
+                  ) as HTMLButtonElement
+                  if (button) {
+                    button.click()
+                  }
+                }}
               >
-                <Stack
-                  p={1}
-                  alignItems={'center'}
-                  justifyContent={'center'}
-                  component={'div'}
+                <TextOnlyTooltip
+                  placement={'left'}
+                  title={t(
+                    'client:sidebar__speed_dial__one_click_prompts__button',
+                  )}
                 >
-                  <MagicBookIcon
-                    sx={{ fontSize: '24px', color: 'primary.main' }}
-                  />
-                </Stack>
-              </TextOnlyTooltip>
-            </Box>
-          }
-          tooltipTitle={''}
-        />}
+                  <Stack
+                    p={1}
+                    alignItems={'center'}
+                    justifyContent={'center'}
+                    component={'div'}
+                  >
+                    <MagicBookIcon
+                      sx={{ fontSize: '24px', color: 'primary.main' }}
+                    />
+                  </Stack>
+                </TextOnlyTooltip>
+              </Box>
+            }
+            tooltipTitle={''}
+          />
+        )}
         <SpeedDialAction
           icon={
             <Box

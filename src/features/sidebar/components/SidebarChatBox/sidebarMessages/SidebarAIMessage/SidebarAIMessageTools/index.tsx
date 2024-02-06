a36c63782393d692393341aa8e7c7335473435c5
yanggerSimply <i@yangger.cn>
2024-02-06 23:21:06 +0800

    fix: error.

--- src/features/sidebar/components/SidebarChatBox/sidebarMessages/SidebarAIMessage/SidebarAIMessageTools/index.tsx
+++ src/features/sidebar/components/SidebarChatBox/sidebarMessages/SidebarAIMessage/SidebarAIMessageTools/index.tsx
@@ -80,6 +80,9 @@ const SidebarAIMessageTools: FC<{
                     })
                   }
                 }
+                newImage.onerror = () => {
+                  resolve(false)
+                }
               })
             } catch (e) {
               console.error('Error copying image to clipboard', e)

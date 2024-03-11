dd66094f92171e3b05db2b0f39af1215b108b937
yangger <chenyang@simplylab.xyz>
2024-03-11 15:06:41 +0800

    chore: fix props error and pref code.

--- src/components/CopyTooltipIconButton.tsx
+++ src/components/CopyTooltipIconButton.tsx
@@ -1,14 +1,16 @@
 import ContentCopyIcon from '@mui/icons-material/ContentCopy'
 import { IconButtonProps } from '@mui/material/IconButton'
 import { SxProps } from '@mui/material/styles'
-import React, { type FC, useMemo, useState, type ReactNode } from 'react'
+import React, { type FC, type ReactNode, useMemo, useState } from 'react'
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 import { useTranslation } from 'react-i18next'
 
 import { ContextMenuIcon } from '@/components/ContextMenuIcon'
-import TooltipIconButton, { type ITooltipIconButton } from '@/components/TooltipIconButton'
+import TooltipIconButton, {
+  type ITooltipIconButton,
+} from '@/components/TooltipIconButton'
 import { I18nextKeysType } from '@/i18next'
 
 interface ICopyTooltipIconButton extends IconButtonProps {
@@ -57,7 +59,14 @@ const CopyTooltipIconButton: FC<ICopyTooltipIconButton> = (props) => {
         }, 1000)
       }}
     >
-      <TooltipIconButton title={title} sx={sx} className={props.className} TooltipProps={TooltipProps} PopperProps={PopperProps} {...buttonProps}>
+      <TooltipIconButton
+        title={title}
+        sx={sx}
+        className={props.className}
+        TooltipProps={TooltipProps}
+        PopperProps={PopperProps}
+        {...buttonProps}
+      >
         {isCopied ? (
           <ContextMenuIcon icon={'Check'} />
         ) : (

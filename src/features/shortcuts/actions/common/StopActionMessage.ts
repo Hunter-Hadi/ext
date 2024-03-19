279fa70dff26a5a2c4945967837831e73903a706
laizeping <laizeping@simplylab.xyz>
2024-03-19 21:08:18 +0800

    feature(src/features/sidebar/utils/summaryActionsChangeTool): 修复stop无法停止的问题

--- src/features/shortcuts/actions/common/StopActionMessage.ts
+++ src/features/shortcuts/actions/common/StopActionMessage.ts
@@ -5,56 +5,60 @@ import { IShortcutEngineExternalEngine } from '../../types'
 export const stopActionMessage = async (params: {
   engine: IShortcutEngineExternalEngine
 }) => {
-  const currentConversation = await params.engine.clientConversationEngine?.getCurrentConversation()
-  if (currentConversation && currentConversation.id) {
-    const conversationUpdatedDate = +new Date(currentConversation.updated_at)
-    let lastUpdateMessageId = currentConversation.messages?.[0]?.messageId
-    if (currentConversation.messages.length > 1) {
-      //大于1，利用时间进行排序，一般大于1只有ask,不过过多的进入进行sort浪费资源
-      lastUpdateMessageId = currentConversation.messages.sort((a, b) => {
-        if (a.updated_at === undefined) return 1
-        if (b.updated_at === undefined) return -1
-        const dateA = +new Date(a.updated_at)
-        const dateB = +new Date(b.updated_at)
-        // 计算每个消息更新时间与会话更新时间的时间差
-        const diffA = Math.abs(dateA - conversationUpdatedDate)
-        const diffB = Math.abs(dateB - conversationUpdatedDate)
-        // 将时间差更小（即更接近会话更新时间）的消息排在前面
-        return diffA - diffB
-      })?.[0]?.messageId
-    }
+  try {
+    const currentConversation = await params.engine.clientConversationEngine?.getCurrentConversation()
+    if (currentConversation && currentConversation.id) {
+      const conversationUpdatedDate = +new Date(currentConversation.updated_at)
+      let lastUpdateMessageId = currentConversation.messages?.[0]?.messageId
+      if (currentConversation.messages.length > 1) {
+        //大于1，利用时间进行排序，一般大于1只有ask,不过过多的进入进行sort浪费资源
+        lastUpdateMessageId = currentConversation.messages.sort((a, b) => {
+          if (a.updated_at === undefined) return 1
+          if (b.updated_at === undefined) return -1
+          const dateA = +new Date(a.updated_at)
+          const dateB = +new Date(b.updated_at)
+          // 计算每个消息更新时间与会话更新时间的时间差
+          const diffA = Math.abs(dateA - conversationUpdatedDate)
+          const diffB = Math.abs(dateB - conversationUpdatedDate)
+          // 将时间差更小（即更接近会话更新时间）的消息排在前面
+          return diffA - diffB
+        })?.[0]?.messageId
+      }
 
-    //停止的消息状态变更
-    if (currentConversation.id && lastUpdateMessageId) {
-      await clientChatConversationModifyChatMessages(
-        'update',
-        currentConversation.id,
-        0,
-        [
-          {
-            type: 'ai',
-            messageId: lastUpdateMessageId,
-            originalMessage: {
-              metadata: {
-                sources: {
-                  status: 'complete',
+      //停止的消息状态变更
+      if (currentConversation.id && lastUpdateMessageId) {
+        await clientChatConversationModifyChatMessages(
+          'update',
+          currentConversation.id,
+          0,
+          [
+            {
+              type: 'ai',
+              messageId: lastUpdateMessageId,
+              originalMessage: {
+                metadata: {
+                  sources: {
+                    status: 'complete',
+                  },
+                  copilot: {
+                    steps: [
+                      {
+                        title: 'Analyzing video',
+                        status: 'complete',
+                        icon: 'SmartToy',
+                        value: '',
+                      },
+                    ],
+                  },
+                  isComplete: true,
                 },
-                copilot: {
-                  steps: [
-                    {
-                      title: 'Analyzing video',
-                      status: 'complete',
-                      icon: 'SmartToy',
-                      value: '',
-                    },
-                  ],
-                },
-                isComplete: true,
               },
-            },
-          } as any,
-        ],
-      )
+            } as any,
+          ],
+        )
+      }
     }
+  } catch (e) {
+    console.log('stopActionMessage', e)
   }
 }

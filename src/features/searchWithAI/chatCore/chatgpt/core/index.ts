89917ca263302883152f03a942bee33fbb187d92
yangger <chenyang@simplylab.xyz>
2024-03-21 12:05:46 +0800

    fix: chatgpt webapp error.

--- src/features/searchWithAI/chatCore/chatgpt/core/index.ts
+++ src/features/searchWithAI/chatCore/chatgpt/core/index.ts
@@ -289,7 +289,8 @@ export class ChatGPTConversation {
         'GET',
         `/backend-api/conversation/${this.conversationId}`,
       )
-      const chatGPTConversationRaw: IChatGPTConversationRaw = await result.json()
+      const chatGPTConversationRaw: IChatGPTConversationRaw =
+        await result.json()
       this.conversationInfo = {
         title: chatGPTConversationRaw.title,
         messages: mappingToMessages(
@@ -344,6 +345,13 @@ export class ChatGPTConversation {
       parent_message_id: parentMessageId,
       timezone_offset_min: new Date().getTimezoneOffset(),
       history_and_training_disabled: false,
+      force_paragen: false,
+      force_rate_limit: false,
+      conversation_mode: {
+        kind: 'primary_assistant',
+      },
+      websocket_request_id: uuidV4(),
+      suggestions: [],
     } as any
     if (this.conversationId) {
       postMessage.conversation_id = this.conversationId
@@ -373,7 +381,8 @@ export class ChatGPTConversation {
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${this.token}`,
-      },
+        'Openai-Sentinel-Arkose-Token': searchWithAICacheArkoseToken || '',
+      } as any,
       body: JSON.stringify(Object.assign(postMessage)),
       onMessage: (message: string) => {
         console.debug('sse message', message)
@@ -541,11 +550,9 @@ export class ChatGPTDaemonProcess implements IChatGPTDaemonProcess {
     if (this.models.length > 0) {
       return this.models
     }
-    const resp = await chatGptRequest(
-      token,
-      'GET',
-      '/backend-api/models',
-    ).then((r) => r.json())
+    const resp = await chatGptRequest(token, 'GET', '/backend-api/models').then(
+      (r) => r.json(),
+    )
     if (resp?.models && resp.models.length > 0) {
       this.models = resp.models
     }

bd78a11755da0140822167365c2a177e1686d5da
chenyang <chenyang@simplylab.xyz>
2024-04-11 20:05:51 +0800

    feta: supported gemini-1.5-pro(missing doc)

--- src/background/src/chat/MaxAIGeminiChat/types.ts
+++ src/background/src/chat/MaxAIGeminiChat/types.ts
@@ -17,4 +17,19 @@ export const MAXAI_GENMINI_MODELS: IAIProviderModel[] = [
     },
     uploadFileConfig: MAXAI_VISION_MODEL_UPLOAD_CONFIG,
   },
+  {
+    title: 'gemini-1.5-pro',
+    titleTag: '',
+    value: 'gemini-1.5-pro',
+    maxTokens: 1000 * 1000,
+    tags: ['Beta', 'Vision'],
+    poweredBy: 'Google',
+    description: (t) =>
+      t(`client:provider__gemini__model__gemini_pro__description`),
+    permission: {
+      sceneType: 'MAXAI_PAID_MODEL_GEMINI_PRO',
+      roles: ['elite'],
+    },
+    uploadFileConfig: MAXAI_VISION_MODEL_UPLOAD_CONFIG,
+  },
 ]

979786c8ae6ef11635a1776625f8faa908bebae2
zhouru1bin <109497098+zhouru1bin@users.noreply.github.com>
2024-04-23 22:19:52 +0800

    chore: refine log type with model nam

--- src/features/auth/utils/log.ts
+++ src/features/auth/utils/log.ts
@@ -14,22 +14,41 @@ const permissionSceneTypeToLogType = async (
   sceneType: PermissionWrapperCardSceneType,
 ): Promise<string> => {
   let name: string = sceneType
+
+  // search with ai maxai_claude
+  if (sceneType === 'SEARCH_WITH_AI_CLAUDE') {
+    return `SEARCH_WITH_AI(${SEARCH_WITH_AI_DEFAULT_MODEL_BY_PROVIDER['MAXAI_CLAUDE']})`
+  }
+  // search with ai chatgpt
+  if (sceneType === 'SEARCH_WITH_AI_CHATGPT') {
+    return `SEARCH_WITH_AI(${SEARCH_WITH_AI_DEFAULT_MODEL_BY_PROVIDER['USE_CHAT_GPT_PLUS']})`
+  }
+
+  const { sidebarSettings, thirdProviderSettings } =
+    await getChromeExtensionLocalStorage()
+
+  const currentProvider = sidebarSettings?.common?.currentAIProvider || ''
+  const currentModelName =
+    currentProvider && thirdProviderSettings
+      ? thirdProviderSettings[currentProvider]?.model ?? ''
+      : ''
+
   // summary
   if (sceneType === 'PAGE_SUMMARY') {
     const pageSummaryType = getPageSummaryType()
     name = 'SUMMARY'
     switch (pageSummaryType) {
       case 'PDF_CRX_SUMMARY':
-        name += '(PDF)'
+        name += `(PDF ${currentModelName})`
         break
       case 'PAGE_SUMMARY':
-        name += '(DEFAULT)'
+        name += `(DEFAULT ${currentModelName})`
         break
       case 'YOUTUBE_VIDEO_SUMMARY':
-        name += '(YOUTUBE)'
+        name += `(YOUTUBE ${currentModelName})`
         break
       case 'DEFAULT_EMAIL_SUMMARY':
-        name += '(EMAIL)'
+        name += `(EMAIL ${currentModelName})`
         break
       default:
         break
@@ -45,13 +64,13 @@ const permissionSceneTypeToLogType = async (
   ) {
     switch (sceneType) {
       case 'GMAIL_CONTEXT_MENU':
-        name = 'INSTANT_REPLY(REFINE)'
+        name = `INSTANT_REPLY(Refine ${currentModelName})`
         break
       case 'GMAIL_DRAFT_BUTTON':
-        name = 'INSTANT_REPLY(COMPOSE)'
+        name = `INSTANT_REPLY(Compose ${currentModelName})`
         break
       case 'GMAIL_REPLY_BUTTON':
-        name = 'INSTANT_REPLY(REPLY)'
+        name = `INSTANT_REPLY(Reply ${currentModelName})`
         break
       default:
         break
@@ -60,7 +79,6 @@ const permissionSceneTypeToLogType = async (
   }
   // instant reply
   // 判断是否是 instant reply 付费卡点
-
   if (
     sceneType.includes('REFINE_DRAFT_BUTTON') ||
     sceneType.includes('COMPOSE_NEW_BUTTON') ||
@@ -68,42 +86,26 @@ const permissionSceneTypeToLogType = async (
   ) {
     name = 'INSTANT_REPLY'
     if (sceneType.includes('COMPOSE_REPLY_BUTTON')) {
-      name += '(REPLY)'
+      name += `(Reply ${currentModelName})`
       return name
     }
     if (sceneType.includes('REFINE_DRAFT_BUTTON')) {
-      name += '(REFINE)'
+      name += `(Refine ${currentModelName})`
       return name
     }
     if (sceneType.includes('COMPOSE_NEW_BUTTON')) {
-      name += '(COMPOSE)'
+      name += `(Compose ${currentModelName})`
       return name
     }
   }
-
-  // search with ai maxai_claude
-  if (sceneType === 'SEARCH_WITH_AI_CLAUDE') {
-    return `SEARCH_WITH_AI(${SEARCH_WITH_AI_DEFAULT_MODEL_BY_PROVIDER['MAXAI_CLAUDE']})`
-  }
-  // search with ai chatgpt
-  if (sceneType === 'SEARCH_WITH_AI_CHATGPT') {
-    return `SEARCH_WITH_AI(${SEARCH_WITH_AI_DEFAULT_MODEL_BY_PROVIDER['USE_CHAT_GPT_PLUS']})`
-  }
-
-  const { sidebarSettings, thirdProviderSettings } =
-    await getChromeExtensionLocalStorage()
 
   // sidebar search
   if (sceneType === 'SIDEBAR_SEARCH_WITH_AI') {
     name = 'SEARCH'
-    name += sidebarSettings?.search?.copilot ? '(PRO)' : '(QUICK)'
+    const isProSearch = sidebarSettings?.search?.copilot ? 'Pro' : 'Default'
+    name += `(${isProSearch} ${currentModelName})`
     return name
   }
-  const currentProvider = sidebarSettings?.common?.currentAIProvider || ''
-  const currentModelName =
-    currentProvider && thirdProviderSettings
-      ? thirdProviderSettings[currentProvider]?.model ?? ''
-      : ''
 
   // Fast Text Model
   if (sceneType === 'MAXAI_FAST_TEXT_MODEL') {
@@ -122,7 +124,10 @@ const permissionSceneTypeToLogType = async (
     sceneType === 'SIDEBAR_ART_AND_IMAGES' ||
     sceneType === 'MAXAI_IMAGE_GENERATE_MODEL'
   ) {
-    name = `IMAGE_MODEL(${currentModelName})`
+    const isPro = sidebarSettings?.art?.isEnabledConversationalMode
+      ? 'Default'
+      : 'Pro'
+    name = `IMAGE_MODEL(${isPro} ${currentModelName})`
     return name
   }
 

72214158003a4b474e461f4a8b7435760d1f62e0
zhouru1bin <109497098+zhouru1bin@users.noreply.github.com>
2024-05-21 11:52:29 +0800

    feat: UnLoginOverlay

--- src/features/chatgpt/components/ChatGPTStatusWrapper.tsx
+++ src/features/chatgpt/components/ChatGPTStatusWrapper.tsx
@@ -1,34 +1,24 @@
 import Box from '@mui/material/Box'
-import Button from '@mui/material/Button'
-import Divider from '@mui/material/Divider'
-import Link from '@mui/material/Link'
 import Paper from '@mui/material/Paper'
 import Stack from '@mui/material/Stack'
-import Typography from '@mui/material/Typography'
 import { FC, useEffect, useMemo } from 'react'
 import React from 'react'
-import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
+import { useRecoilValue, useSetRecoilState } from 'recoil'
 
 import getLiteChromeExtensionDBStorage from '@/background/utils/chromeExtensionStorage/getLiteChromeExtensionDBStorage'
-import { ContextMenuIcon } from '@/components/ContextMenuIcon'
-import { GoogleIcon, UseChatGptIcon } from '@/components/CustomIcon'
-import { APP_USE_CHAT_GPT_HOST } from '@/constants'
-import { AuthState } from '@/features/auth/store'
 import ChatGPTRefreshPageTips from '@/features/chatgpt/components/ChatGPTRefreshPageTips'
-import AIProviderIcon from '@/features/chatgpt/components/icons/AIProviderIcon'
 import ThirdPartAIProviderConfirmDialog from '@/features/chatgpt/components/ThirdPartAIProviderConfirmDialog'
 import { useClientConversation } from '@/features/chatgpt/hooks/useClientConversation'
 import { ThirdPartyAIProviderConfirmDialogState } from '@/features/chatgpt/store'
 import { clientGetConversationStatus } from '@/features/chatgpt/utils'
 import { usePrevious } from '@/features/common/hooks/usePrevious'
-import { mixpanelTrack } from '@/features/mixpanel/utils'
 import { AppDBStorageState } from '@/store'
 import { isMaxAIImmersiveChatPage } from '@/utils/dataHelper/websiteHelper'
+
 // import { IChatGPTProviderType } from '@/background/provider/chat'
 
 const ChatGPTStatusWrapper: FC = () => {
   const { currentConversationId } = useClientConversation()
-  const [authLogin] = useRecoilState(AuthState)
   const setAppDBStorage = useSetRecoilState(AppDBStorageState)
   const { conversationStatus, updateConversationStatus } =
     useClientConversation()
@@ -116,195 +106,6 @@ const ChatGPTStatusWrapper: FC = () => {
     )
   }
 
-  if (!authLogin.isLogin) {
-    return (
-      <Box sx={memoMaskSx}>
-        <Stack width={'calc(100% - 16px)'}>
-          <Stack
-            spacing={1.5}
-            p={3}
-            pb={2}
-            sx={{
-              maxWidth: '414px',
-              mx: 'auto!important',
-              borderRadius: '4px 4px 0 0',
-              width: '100%',
-              bgcolor: (t) =>
-                t.palette.mode === 'dark' ? '#4f4f4f' : '#F5F6F7',
-            }}
-          >
-            <Stack
-              direction={'row'}
-              alignItems={'center'}
-              justifyContent={'center'}
-              width={'100%'}
-              spacing={1}
-            >
-              <UseChatGptIcon sx={{ fontSize: '24px' }} />
-              <Typography
-                fontSize={'24px'}
-                fontWeight={700}
-                color={'text.secondary'}
-                textAlign={'center'}
-              >
-                Use 1-Click AI Anywhere
-              </Typography>
-            </Stack>
-            <Stack
-              direction={'row'}
-              sx={{
-                alignItems: 'center',
-                justifyContent: 'center',
-                gap: 3,
-              }}
-            >
-              <Stack direction={'row'} alignItems={'center'} spacing={1}>
-                <AIProviderIcon
-                  aiProviderType={'USE_CHAT_GPT_PLUS'}
-                  size={20}
-                />
-                <Typography
-                  component={'span'}
-                  fontSize={'16px'}
-                  color={'text.secondary'}
-                >
-                  ChatGPT
-                </Typography>
-              </Stack>
-              <Stack direction={'row'} alignItems={'center'} spacing={1}>
-                <AIProviderIcon aiProviderType={'MAXAI_CLAUDE'} size={20} />
-                <Typography
-                  component={'span'}
-                  fontSize={'16px'}
-                  color={'text.secondary'}
-                >
-                  Claude
-                </Typography>
-              </Stack>
-              <Stack direction={'row'} alignItems={'center'} spacing={1}>
-                <AIProviderIcon aiProviderType={'MAXAI_GEMINI'} size={20} />
-                <Typography
-                  component={'span'}
-                  fontSize={'16px'}
-                  color={'text.secondary'}
-                >
-                  Gemini
-                </Typography>
-              </Stack>
-            </Stack>
-          </Stack>
-          <Paper
-            sx={{
-              maxWidth: '414px',
-              mx: 'auto!important',
-              width: '100%',
-              p: 2,
-              bgcolor: 'background.paper',
-              borderRadius: '0 0 4px 4px',
-            }}
-          >
-            <Stack alignItems={'flex-start'} height={'100%'} spacing={2}>
-              <Stack
-                direction={'row'}
-                alignItems={'center'}
-                justifyContent={'center'}
-                width={'100%'}
-                spacing={1}
-              >
-                <Typography
-                  fontSize={'24px'}
-                  fontWeight={700}
-                  color={'text.primary'}
-                  textAlign={'center'}
-                >
-                  Please sign in to continue
-                </Typography>
-              </Stack>
-              <Link
-                href={APP_USE_CHAT_GPT_HOST + '/login?auto=true'}
-                target={'_blank'}
-                sx={{ width: '100%' }}
-                onClick={() => {
-                  mixpanelTrack('sign_up_started', {
-                    signUpMethod: 'google',
-                    platform: 'extension',
-                  })
-                }}
-              >
-                <Button
-                  fullWidth
-                  disableElevation
-                  variant={'contained'}
-                  sx={{
-                    height: 48,
-                    color: '#fff',
-                    border: 'none',
-                    bgcolor: '#4285F4',
-                    fontSize: 14,
-                    '&:hover': {
-                      bgcolor: '#366dc9',
-                    },
-                  }}
-                >
-                  <Stack direction={'row'} spacing={2} alignItems={'center'}>
-                    <Stack
-                      sx={{
-                        width: '32px',
-                        height: '32px',
-                        borderRadius: '50%',
-                        bgcolor: '#fff',
-                      }}
-                      justifyContent={'center'}
-                      alignItems={'center'}
-                    >
-                      <GoogleIcon sx={{ fontSize: '24px' }} />
-                    </Stack>
-                    <span>Continue with Google</span>
-                  </Stack>
-                </Button>
-              </Link>
-              <Divider sx={{ width: '100%' }}>OR</Divider>
-              <Link
-                href={APP_USE_CHAT_GPT_HOST + '/login-email'}
-                target={'_blank'}
-                sx={{ width: '100%' }}
-                onClick={() => {
-                  mixpanelTrack('sign_up_started', {
-                    signUpMethod: 'email',
-                    platform: 'extension',
-                  })
-                }}
-              >
-                <Button
-                  fullWidth
-                  disableElevation
-                  variant={'outlined'}
-                  sx={{
-                    height: 48,
-                    textIndent: '16px',
-                    fontSize: 14,
-                  }}
-                >
-                  <Stack
-                    direction={'row'}
-                    spacing={2}
-                    alignItems={'center'}
-                    sx={{
-                      color: 'primary.main',
-                    }}
-                  >
-                    <ContextMenuIcon icon={'Email'} sx={{ fontSize: '24px' }} />
-                    <span>Continue with Email</span>
-                  </Stack>
-                </Button>
-              </Link>
-            </Stack>
-          </Paper>
-        </Stack>
-        {memoImmersiveChatMask}
-      </Box>
-    )
-  }
   if (
     conversationStatus === 'needAuth' ||
     conversationStatus === 'loading' ||

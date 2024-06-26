529335bd9d996937b9c01b54d639461ee1a8a068
chenyang <chenyang@simplylab.xyz>
2024-06-26 11:01:29 +0800

    fix: chat with document using MaxAIPromptAction bug

--- src/background/provider/chat/maxai-providers/index.ts
+++ src/background/provider/chat/maxai-providers/index.ts
@@ -271,41 +271,45 @@ export const maxAIAPISendQuestion: IMaxAIAskQuestionFunctionType = async (
     }
     // 当前只有大文件聊天用到这个model
     if (backendAPI === 'chat_with_document') {
-      postBody.model_name = 'gpt-3.5-turbo-16k'
-      if (
-        postBody.prompt_id === postBody.prompt_name &&
-        postBody.prompt_id === 'chat'
-      ) {
-        postBody.prompt_id = 'summary_chat'
-        postBody.prompt_name = 'summary_chat'
+      if (MaxAIPromptActionConfig) {
+        delete postBody.doc_id
+      } else {
+        postBody.model_name = 'gpt-3.5-turbo-16k'
+        if (
+          postBody.prompt_id === postBody.prompt_name &&
+          postBody.prompt_id === 'chat'
+        ) {
+          postBody.prompt_id = 'summary_chat'
+          postBody.prompt_name = 'summary_chat'
+        }
+        const messageContent = postBody.message_content?.find(
+          (content) => content.type === 'text',
+        )
+        const messageContentText = messageContent?.text || ''
+        postBody.message_content = messageContentText as any
+        // 大文件聊天的接口不支持新的message结构，换成老的 - @xiang.xu - 2024-01-15
+        postBody.chat_history = postBody.chat_history?.map((history) => {
+          // 老得结构
+          // {
+          //   type: 'human' | 'ai' | 'generic' | 'system' | 'function'
+          //   data: {
+          //     content: string
+          //     additional_kwargs: {
+          //       [key: string]: any
+          //     }
+          //   }
+          // }
+          return {
+            type: history.role,
+            data: {
+              content:
+                history.content.find((content) => content.type === 'text')
+                  ?.text || '',
+              additional_kwargs: {},
+            },
+          } as any
+        })
       }
-      const messageContent = postBody.message_content?.find(
-        (content) => content.type === 'text',
-      )
-      const messageContentText = messageContent?.text || ''
-      postBody.message_content = messageContentText as any
-      // 大文件聊天的接口不支持新的message结构，换成老的 - @xiang.xu - 2024-01-15
-      postBody.chat_history = postBody.chat_history?.map((history) => {
-        // 老得结构
-        // {
-        //   type: 'human' | 'ai' | 'generic' | 'system' | 'function'
-        //   data: {
-        //     content: string
-        //     additional_kwargs: {
-        //       [key: string]: any
-        //     }
-        //   }
-        // }
-        return {
-          type: history.role,
-          data: {
-            content:
-              history.content.find((content) => content.type === 'text')
-                ?.text || '',
-            additional_kwargs: {},
-          },
-        } as any
-      })
     }
   }
   if (backendAPI === 'get_summarize_response') {

cbf29baee5affbe6526b0280170e1c1b6036550c
chenkun <chenkun@simplylab.xyz>
2024-07-09 10:37:34 +0800

    chore: update i18n

--- src/features/auth/constants/index.ts
+++ src/features/auth/constants/index.ts
@@ -5,7 +5,8 @@ export const PAYING_USER_ROLE_NAME: IUserRoleType[] = ['basic', 'pro', 'elite']
 
 export const AI_MODEL_SERVICE_TEXTS_MAP = {
   fastText: 'GPT-3.5 & Claude-3-haiku & Gemini-pro',
-  advancedText: 'GPT-4 & Claude-3-opus/sonnet & Gemini-1.5-pro',
+  advancedText:
+    'GPT-4o & Claude-3.5-sonnet & Claude-3-opus/sonnet & Gemini-1.5-pro',
   imageGenerate: 'DALL·E 3',
 }
 

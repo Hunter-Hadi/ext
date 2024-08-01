f5a54c7f26677249a82ec1a808302b90dc5ca7d8
chenkun <chenkun@simplylab.xyz>
2024-08-01 15:49:29 +0800

    perf(upload): check file tokens before upload chat file

--- src/features/chatgpt/hooks/upload/useDocUploadAndTextExtraction.ts
+++ src/features/chatgpt/hooks/upload/useDocUploadAndTextExtraction.ts
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from 'react'
+import { useTranslation } from 'react-i18next'
 import { v4 as uuidV4 } from 'uuid'
 
 import { MAXAI_IN_HOUSE_AI_PROVIDERS } from '@/features/chatgpt/constant'
@@ -6,13 +7,17 @@ import useAIProviderUpload, {
   MaxAIAddOrUpdateUploadFile,
 } from '@/features/chatgpt/hooks/upload/useAIProviderUpload'
 import useAIProviderModels from '@/features/chatgpt/hooks/useAIProviderModels'
+import { useClientConversation } from '@/features/chatgpt/hooks/useClientConversation'
 import { IChatUploadFile } from '@/features/indexed_db/conversations/models/Message'
 import { uploadMaxAIDocument } from '@/features/shortcuts/utils/maxAIDocument'
+import { getTextTokensWithRequestIdle } from '@/features/shortcuts/utils/tokenizer'
 import FileExtractor from '@/features/sidebar/utils/FileExtractor'
 import globalSnackbar from '@/utils/globalSnackbar'
 
 const useDocUploadAndTextExtraction = () => {
+  const { t } = useTranslation(['client'])
   const { addOrUpdateUploadFile, getCanUploadFiles } = useAIProviderUpload()
+  const { clientConversation } = useClientConversation()
   const { currentAIProvider, currentAIProviderModel } = useAIProviderModels()
   // 由于 执行 updateAIProviderModel 会导致 aiProviderUploadFiles 更新，
   // 但是 aiProviderUploadFiles 会被缓存，所以这里使用 ref 来获取最新的 aiProviderUploadFiles
@@ -103,31 +108,56 @@ const useDocUploadAndTextExtraction = () => {
                   uploadStatus: 'success',
                   uploadProgress: 80,
                 })
-                const uploadResult = await uploadMaxAIDocument({
-                  file,
-                  pure_text: extractedResult.chatUploadFile.extractedContent,
-                  doc_type: 'chat_file',
-                })
-                if (uploadResult.success) {
-                  uploadResultMap[fileId] = {
-                    uploadStatus: 'success',
-                    uploadProgress: 100,
-                    uploadedFileId: uploadResult.doc_id,
-                    uploadedUrl: uploadResult.doc_url,
-                  }
-                } else {
-                  uploadResultMap[fileId] = {
+                // 如果内容超出当前模型上下文长度，直接报错
+                const { isLimit } = await getTextTokensWithRequestIdle(
+                  extractedResult.chatUploadFile.extractedContent || '',
+                  {
+                    tokenLimit: clientConversation?.meta.maxTokens || 4096,
+                  },
+                )
+                if (isLimit) {
+                  const error = t(
+                    `client:provider__chatgpt__upload_file_error__total_too_large__text`,
+                  )
+                  await addOrUpdateUploadFileRef.current(fileId, {
                     uploadStatus: 'error',
                     uploadProgress: 0,
-                    uploadErrorMessage: uploadResult.error,
-                  }
-                  globalSnackbar.error(uploadResult.error, {
+                    uploadErrorMessage: error,
+                  })
+                  globalSnackbar.error(error, {
                     autoHideDuration: 3000,
                     anchorOrigin: {
                       vertical: 'top',
                       horizontal: 'right',
                     },
                   })
+                } else {
+                  const uploadResult = await uploadMaxAIDocument({
+                    file,
+                    pure_text: extractedResult.chatUploadFile.extractedContent,
+                    doc_type: 'chat_file',
+                  })
+                  if (uploadResult.success) {
+                    uploadResultMap[fileId] = {
+                      uploadStatus: 'success',
+                      uploadProgress: 100,
+                      uploadedFileId: uploadResult.doc_id,
+                      uploadedUrl: uploadResult.doc_url,
+                    }
+                  } else {
+                    uploadResultMap[fileId] = {
+                      uploadStatus: 'error',
+                      uploadProgress: 0,
+                      uploadErrorMessage: uploadResult.error,
+                    }
+                    globalSnackbar.error(uploadResult.error, {
+                      autoHideDuration: 3000,
+                      anchorOrigin: {
+                        vertical: 'top',
+                        horizontal: 'right',
+                      },
+                    })
+                  }
                 }
               } else if (extractedResult.error) {
                 await addOrUpdateUploadFileRef.current(fileId, {
